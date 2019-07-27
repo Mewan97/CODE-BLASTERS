df5a945592708bd80c6fcfeffebdd058c436592c
Hirantha <hiranthapeiris404@gmail.com>
2019-07-27 14:08:53 +0530

    new

--- CodeBlasters.java
+++ CodeBlasters.java
@@ -8,11 +8,8 @@
 package sample;
 
 
-import robocode.HitByBulletEvent;
-import robocode.HitRobotEvent;
-import robocode.AlphaBot;
-import robocode.ScannedRobotEvent;
-
+import robocode.*;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 import java.awt.*;
 
 
@@ -25,72 +22,122 @@ import java.awt.*;
  * @author Mathew A. Nelson (original)
  * @author Flemming N. Larsen (contributor)
  */
-public class CodeBlasters extends AlphaBot{
-    int turnDirection = 120; // Clockwise or counterclockwise
-    int dist = 50; // distance to move when we're hit
+public class CodeBlasters extends BravoBot
+{
     /**
-     * run: Spin around looking for a target
+     * run: CodeBlasters's default behavior
      */
-    public void run() {
-        // Set colors
-        setBodyColor(Color.lightGray);
-        setGunColor(Color.gray);
-        setRadarColor(Color.darkGray);
 
-        // Spin the gun around slowly... forever
-        while (true) {
-            turnRight(5 * turnDirection);
+	/*boolean peek; // Don't turn if there's a robot there
+	double moveAmount;*/
+    int turnDirection = 1;
+
+    public void run() {
+        // Initialization of the robot should be put here
+
+        // After trying out your robot, try uncommenting the import at the top,
+        // and the next line:
+
+        // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+        // Robot main loop
+
+		/*peek = false;
+		turnLeft(getHeading() % 90);
+		ahead(moveAmount);
+		peek = true;
+		turnGunRight(90);
+		turnRight(90);
+		while (true) {
+			// Look before we turn when ahead() completes.
+			peek = true;
+			// Move up the wall
+			ahead(moveAmount);
+			// Don't look now
+			peek = false;
+			// Turn to the next wall
+			turnRight(90);
+		}*/
+
+
+        while(true) {
+            // Replace the next 4 lines with any behavior you would like
+            ahead(50);
+            turnRight(120 * turnDirection);
+
         }
+
     }
 
-
     /**
-     * onScannedRobot:  Fire!
+     * onScannedRobot: What to do when you see another robot
      */
     public void onScannedRobot(ScannedRobotEvent e) {
+        // Calculate exact location of the robot
+        double absoluteBearing = getHeading() + e.getBearing();
+        double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+
         if (e.getBearing() >= 0) {
             turnDirection = 1;
         } else {
             turnDirection = -1;
         }
+        // If it's close enough, fire!
+        if (Math.abs(bearingFromGun) <= 3) {
 
-        turnRight(e.getBearing());
-        ahead(e.getDistance() + 5);
-        scan(); // Might want to move ahead again!
+            turnRight(bearingFromGun*turnDirection);
+            // We check gun heat here, because calling fire()
+            // uses a turn, which could cause us to lose track
+            // of the other robot.
+            if (getGunHeat() == 0) {
+                fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+            }
+        } // otherwise just set the gun to turn.
+        // Note:  This will have no effect until we call scan()
+        else {
+            turnRight(bearingFromGun);
+        }
+        // Generates another scan event if we see a robot.
+        // We only need to call this if the gun (and therefore radar)
+        // are not turning.  Otherwise, scan is called automatically.
+        if (bearingFromGun == 0) {
+            scan();
+        }// Might want to move ahead again!
     }
 
+
+
     /**
-     * onHitByBullet:  Turn perpendicular to the bullet, and move a bit.
+     * onHitByBullet: What to do when you're hit by a bullet
      */
     public void onHitByBullet(HitByBulletEvent e) {
+        // Replace the next line with any behavior you would like
+        double angle = e.getBearing();
+        turnRight(-angle);
+        ahead(100);
 
-        if (getEnergy() > 16) {
-            fire(3);
-        } else if (getEnergy() > 10) {
-            fire(2);
-        } else if (getEnergy() > 4) {
-            fire(1);
-        } else if (getEnergy() > 2) {
-            fire(.5);
-        } else if (getEnergy() > .4) {
-            fire(.1);
-        }
-        ahead(40);
     }
 
     /**
-     * onHitRobot:  Aim at it.  Fire Hard!
+     * onHitWall: What to do when you hit a wall
      */
-    public void onHitRobot(HitRobotEvent e) {
-        if (e.getBearing() >= 0) {
-            turnDirection = 1;
-        } else {
-            turnDirection = -1;
-        }
-        turnRight(e.getBearing());
+    public void onHitWall(HitWallEvent e) {
+        // Replace the next line with any behavior you would like
 
-        // Determine a shot that won't kill the robot...
-        // We want to ram him instead for bonus points
+        double energy = getEnergy();
+
+        double bearing = e.getBearing(); //Get the direction which is arrived the bullet.
+        if(energy < 100){ // if the energy is low, the robot go away from the enemy
+            turnRight(-bearing); //This isn't accurate but release your robot.
+            ahead(100); //The robot goes away from the enemy.
+        }
+        else{
+            turnRight(360);
+            ahead(50);
+        }
+
+    }
+    public void fireAt(HitRobotEvent e){
         if (e.getEnergy() > 16) {
             fire(3);
         } else if (e.getEnergy() > 10) {
@@ -102,6 +149,12 @@ public class CodeBlasters extends AlphaBot{
         } else if (e.getEnergy() > .4) {
             fire(.1);
         }
-        ahead(40); // Ram him again!
+    }
+    public void onHitRobot(HitRobotEvent e){
+        double angle = e.getBearing();
+        turnRight(angle);
+        fireAt(e);
+        back(50);
+
     }
 }
