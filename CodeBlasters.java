ea8e57d2e4c5ebabd84901be25d9b9d86fb95dbd
Mewan97 <damithamewan201@gmail.com>
2019-07-27 16:18:33 +0530

    asdas

--- CodeBlasters.java
+++ CodeBlasters.java
@@ -1,9 +1,31 @@
+/**
+ * Copyright (c) 2001-2019 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://robocode.sourceforge.io/license/epl-v10.html
+ */
 
 import robocode.*;
 
-public class CodeBlasters extends CharlieBot {
-	int turnDirection = 1;
+import java.awt.*;
 
+
+/**
+ * RamFire - a sample robot by Mathew Nelson.
+ * <p>
+ * Drives at robots trying to ram them.
+ * Fires when it hits them.
+ *
+ * @author Mathew A. Nelson (original)
+ * @author Flemming N. Larsen (contributor)
+ */
+public class CodeBlasters extends CharlieBot {
+	int turnDirection = 1; // Clockwise or counterclockwise
+
+	/**
+	 * run: Spin around looking for a target
+	 */
 	public void run() {
 		
 
@@ -14,7 +36,11 @@ public class CodeBlasters extends CharlieBot {
 		}
 	}
 
-	public void onRobotDetected(ScannedRobotEvent e){
+	/**
+	 * onScannedRobot:  We have a target.  Go get it.
+	 */
+	/*public void onScannedRobot(ScannedRobotEvent e) {
+
 		if (e.getBearing() >= 0) {
 			turnDirection = 1;
 		} else {
@@ -23,9 +49,27 @@ public class CodeBlasters extends CharlieBot {
 
 		turnRight(e.getBearing());
 		ahead(e.getDistance() + 5);
+		scan(); // Might want to move ahead again!
+	}*/
+
+	public void onRobotDetected(ScannedRobotEvent e){
+
+		if (e.getBearing() >= 0) {
+			turnDirection = 1;
+		} else {
+			turnDirection = -1;
+		}
+
+		turnRight(e.getBearing());
+		if(getNumSentries() > 6)
+			fire(1);
+		ahead(e.getDistance() + 5);
 		scan();
 	}
-	
+
+	/**
+	 * onHitRobot:  Turn to face robot, fire hard, and ram him again!
+	 */
 	public void onHitRobot(HitRobotEvent e) {
 		if (e.getBearing() >= 0) {
 			turnDirection = -1;
@@ -35,10 +79,12 @@ public class CodeBlasters extends CharlieBot {
 	
 		turnRight(e.getBearing());
 
+		// Determine a shot that won't kill the robot...
+		// We want to ram him instead for bonus points
 		if (e.getEnergy() > 16) {
-			fire(3);
+			fire(4);
 		} else if (e.getEnergy() > 10) {
-			fire(2);
+			fire(3);
 		} else if (e.getEnergy() > 4) {
 			fire(1);
 		} else if (e.getEnergy() > 2) {
@@ -46,6 +92,16 @@ public class CodeBlasters extends CharlieBot {
 		} else if (e.getEnergy() > .4) {
 			fire(.1);
 		}
-		ahead(100);
+		ahead(100); // Ram him again!
+	}
+	
+	public void onBulletHit(BulletHitEvent e){
+		
+		double energy = getEnergy();
+		
+		if(energy < 50){
+			turnRight(45);
+			back(20);
+		}
 	}
 }
